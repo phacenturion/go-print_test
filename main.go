3f50f0ee1bb25dda0b82df4a22408199282624f9
phacenturion <pedro.ha.centurion@gmail.com>
2022-04-24 20:06:43 -0300

    teste

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func print_test() {
+func Print_test() {
 	fmt.Println("Hello world!")
 }
-func str_test() string {
+func Str_test() string {
 	return "another"
 }
 func main() {
-	print(str_test())
+	print(Str_test())
 }
